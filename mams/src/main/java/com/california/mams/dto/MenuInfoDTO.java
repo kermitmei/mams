3990ab8360a2b2313bb93b7fb8ab3a9464ef84f5
John Doe <johndoeabUdc@aexample.com>
2018-11-09 13:44:41 +0800

    修改配置，解决测试用例无法运行的问题

--- mams/src/main/java/com/california/mams/dto/MenuInfoDTO.java
+++ mams/src/main/java/com/california/mams/dto/MenuInfoDTO.java
@@ -1,75 +1,9 @@
-package com.california.common.dto;
+package com.california.mams.dto;
 
-import com.california.mams.orm.model.MamsMenu;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 
 /**
  * Created by ffn on 8/11/2018.
  */
 public class MenuInfoDTO {
-    public class MenuItem {
-        String kid;
-        String url;
-        String name;
-        String comment;
 
-        public MenuItem(String hostUrl, MamsMenu menu) {
-            this.kid = menu.getKid();
-            this.url = hostUrl + menu.getUri();
-            this.name = menu.getName();
-            this.comment = menu.getComment();
-        }
-
-        public String getKid() {
-            return kid;
-        }
-
-        public void setKid(String kid) {
-            this.kid = kid;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
-
-    private List<MenuItem> menuList;
-
-    public MenuInfoDTO(String hostUrl, List<MamsMenu> mamsMenuList) {
-        this.menuList = new ArrayList<>();
-        menuList.sort(Comparator.comparing(MamsMenu::getWeight));
-        List<MamsMenu> tmpMenuList = new ArrayList<>();
-        for (MamsMenu menu : mamsMenuList) {
-            if (StringUtils.isEmpty(menu.getP1Kid())) {
-                tmpMenuList.add(menu);
-                menuList.add(new MenuItem(hostUrl, menu));
-            }
-        }
-        mamsMenuList.removeAll(tmpMenuList);
-
-    }
-
-    public List<MenuItem> getMenuList() {
-        return menuList;
-    }
-
-    public void setMenuList(List<MenuItem> menuList) {
-        this.menuList = menuList;
-    }
 }
--- mams/src/test/java/com/california/mams/service/MamsServiceTest.java
+++ mams/src/test/java/com/california/mams/service/MamsServiceTest.java
@@ -1,8 +1,69 @@
+package com.california.mams.service;
+
+import com.california.mams.BaseTest;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by ffn on 8/11/2018.
+ * Created by ffn on 9/11/2018.
  */
-public class MamsServiceTest {
+public class MamsServiceTest extends BaseTest {
 
+    @Test
+    public void createGroup() {
+        logger.info("Test");
+    }
+
+    @Test
+    public void createAuth() {
+    }
+
+    @Test
+    public void createMenu() {
+    }
+
+    @Test
+    public void updateGroup() {
+    }
+
+    @Test
+    public void addMembersToGroup() {
+    }
+
+    @Test
+    public void addAuthsToGroup() {
+    }
+
+    @Test
+    public void addMenusToGroup() {
+    }
+
+    @Test
+    public void deleteGroup() {
+    }
+
+    @Test
+    public void deleteAuth() {
+    }
+
+    @Test
+    public void deleteMenu() {
+    }
+
+    @Test
+    public void authenticate() {
+    }
+
+    @Test
+    public void getGroupsContainMember() {
+    }
+
+    @Test
+    public void getMenusOfMember() {
+    }
+
+    @Test
+    public void getAuthsOfMember() {
+    }
 }

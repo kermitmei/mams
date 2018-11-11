130baf30acab4308e8290f6485d14fa6056a5e9a
John Doe <johndoeabUdc@aexample.com>
2018-11-11 09:45:23 +0800

    0.0.8发布成功

--- mams/build.sh
+++ mams/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.0.6
+VERSION=0.0.8
 gradle mamsLib
 mvn install:install-file -DgroupId=com.california -DartifactId=mams -Dversion=$VERSION -Dfile=/Users/kermit/Projects/mams/mams/build/libs/mams-$VERSION.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=/Users/kermit/Projects/tmp/public-maven-repo  -DcreateChecksum=true
 
